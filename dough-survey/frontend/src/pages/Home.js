940327f84b9eaddd576b16bdf59b7a13a6c71870
kjwispro <kjwispro@gmail.com>
2021-11-03 11:50:04 +0900

    211103 main-page Collapse

--- dough-survey/frontend/src/pages/Home.js
+++ dough-survey/frontend/src/pages/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Home.css';
 import Header from '../components/Header'
 import SlideImages from '../components/SlideImages'
+import CollapseResult from '../components/CollapseResult'
 
 function Home(props) {
   return (
     <div className="Home-page">
       <Header changeIsHome={props.changeIsHome} />
-      <div className="sampleComp" style={{background: "thistle"}}>HomeComponent #1</div>
+      <CollapseResult content="helloWorld"/>
       <SlideImages />
       <div className="sampleComp" style={{background: "silver"}}>HomeComponent #3</div>
       <div className="sampleComp" style={{background: "orange"}}>HomeComponent #4</div>
