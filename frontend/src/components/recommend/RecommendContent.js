2db12c709790817b90f74e39876e35d0f36588dd
kjwispro <kjwispro@gmail.com>
2021-12-11 00:18:15 +0900

    211210 share button test

--- frontend/src/components/recommend/RecommendContent.js
+++ frontend/src/components/recommend/RecommendContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './RecommendContent.css';
 
 import SlideImages from '../common/SlideImages';
@@ -11,11 +11,29 @@ function RecommendContent(props) {
     const elem = props.elem;
     const index = props.index;
     const target = props.elem.target;
-
+    
     function openListPage () {
         props.openListPage();
         props.setListPageContents({name: elem.mainTitle});
     }
+    
+    useEffect (() => {
+        var shareButton = document.getElementById("shareButton");
+        shareButton.addEventListener('click', function (event) {
+            if (navigator.share){
+                navigator.share({
+                    title: "Web Share API",
+                    url: "http://localhost:3000/main"
+                }).then(() => {
+                    console.log('share done');
+                })
+                .catch(console.error)
+            }
+            else {
+                console.log('fail');
+            }
+        })
+    },[])
 
     return (
         <div className={`recommendContent ${target?"specific":""}`} key={index}>
@@ -28,7 +46,7 @@ function RecommendContent(props) {
                     <div className="icon" onClick={openListPage}><Chevron width={"0.4em"} color={"rgba(0,0,0,0.9)"}/></div>
                 </div>
                 <div className="subTitle">{elem.subTitle}</div>
-                <div className="icon"><ShareIcon width={20} color={"rgba(0,0,0,0.9)"} /></div>
+                <div className="icon" id="shareButton"><ShareIcon width={20} color={"rgba(0,0,0,0.9)"} /></div>
             </div>
             <SlideImages 
                 openModal={props.openShopPage} 
