ee8f30e946d5c82b03f7db6f5d1950fab395271b
AnHaechan <mike000529@gmail.com>
2021-12-30 16:28:31 +0900

    refactored main.py

--- crawler/main.py
+++ crawler/main.py
@@ -1,29 +1,27 @@
 from dough_crawler import *
 from firestore_lib import *
-import time
-import csv
 
-stations = ['강남역', '뚝섬역', '합정역']
-search_keywords = ['맛집', '카페', '술집']
-crawler_options = dict(log=True, msg=True)
-done_list = os.listdir('./raw_db')
+def crawl(stations, search_keywords, crawler_options, done_list):
+    dhc = DoughCrawler()
+    for station_name in stations:
+        for search_keyword in search_keywords:
+            if f'{station_name}_{search_keyword}' in done_list:
+                continue
+            else:
+                dhc.run_crawler_naver(station_name=station_name, search_keyword=search_keyword, options=crawler_options)
 
-dhc = DoughCrawler()
-for station_name in stations:
-    for search_keyword in search_keywords:
-        if f'{station_name}_{search_keyword}' in done_list:
-            continue
-        else:
-            dhc.run_crawler_naver(station_name=station_name, search_keyword=search_keyword, options=crawler_options)
-            # test code for category analysis
-            """
-            db_list = dhc.get_db_list()
-            category_csv_path = f'category_output/{station_name}_{search_keyword}.csv'
-            f = open(category_csv_path, 'w+', encoding='ANSI', newline='')
-            wr = csv.writer(f)
-            for item in db_list:
-                wr.writerow([item.get_value('place_name'), item.get_value('place_category')])
-            f.close()
-            """
+def upload_to_db(raw_db):
+    raise NotImplementedError
+    # TODO
+    # (1) open `./raw_db` and start converting & uploading
+    # raw_db is the file dumped with `dill`
 
+if __name__ == "__main__":
+    stations = ['강남역', '뚝섬역', '합정역']
+    search_keywords = ['맛집', '카페', '술집']
+    crawler_options = dict(log=True, msg=True)
+    done_list = os.listdir('./raw_db')
 
+    #crawl(stations, search_keywords, crawler_options, done_list)
+
+    upload_to_db(raw_db=done_list)
