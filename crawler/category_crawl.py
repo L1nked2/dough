da199c504e79ffeb22db6d7729ff42898ac446ca
AnHaechan <mike000529@gmail.com>
2022-01-26 16:27:24 +0900

    220126 crawler : category_crawler 'save_only_diff' implemented

--- crawler/category_crawl.py
+++ crawler/category_crawl.py
@@ -2,25 +2,26 @@
 file for category crawling.
 """
 import os, pickle
-from utils import csv_to_list, create_dirs_if_not_exist
+
+from numpy import iterable
+from utils import csv_to_list, iterable_to_csv, create_dirs_if_not_exist
 from dough_crawler import crawl_only_category
 
 def save_only_diff(collected_category_dir_path, existing_category_to_tag_table_dir_path):
+  for kind, kind_eng in [('술집', 'drink'), ('맛집', 'res'), ('카페','cafe')]:
+    # union all the collected categories
+    all_collected_categories = set()
+    for entry in os.listdir(collected_category_dir_path):
+      station_type_pickle = os.path.join(collected_category_dir_path, entry)
+      if not station_type_pickle.endswith(f'_{kind}.pkl'): continue 
+    
+      collected_categories_for_current_station_type = pickle.load(open(station_type_pickle, "rb")) # e.g. category set colleted for 강남역_맛집
+      all_collected_categories.update(collected_categories_for_current_station_type)
 
-  ##### for kind in ['술집', '맛집', '카페] --> 각각 kind 마다 all_collected_categories 하나, diff csv 하나 있어야 함
-  
-  all_collected_categories = set()
-  for entry in os.listdir(collected_category_dir_path):
-    station_type_pickle = os.path.join(collected_category_dir_path, entry)
-    if not station_type_pickle.endswith('.pkl'): continue 
-  
-    collected_categories_for_current_station_type = pickle.load(open(station_type_pickle, "rb")) # e.g. category set colleted for 강남역_맛집
-    assert isinstance(collected_categories_for_current_station_type, set)
-    all_collected_categories.update(collected_categories_for_current_station_type)
-
-  # open and set difference --> into csv
-
-  pass 
+    # open existing category csv and compute difference
+    existing_categories = set(csv_to_list(f"{existing_category_to_tag_table_dir_path}/category_to_tag_table_{kind_eng}.csv", encoding='cp949')[1:])
+    new_categories = all_collected_categories.difference(existing_categories)
+    iterable_to_csv(new_categories, csv_path=f"{existing_category_to_tag_table_dir_path}/new_cat_{kind}.csv")
 
 
 if __name__ == "__main__":
@@ -36,8 +37,12 @@ if __name__ == "__main__":
     CRAWLER_OPTIONS = dict(log=True, msg=True)
     CRAWL_ONLY_TEN_PLACES_FOR_TEST = False
 
-    crawl_only_category(STATIONS, SEARCH_KEYWORD, CRAWLER_OPTIONS, 
-    DB_DIR_PATH, PHOTO_DIR_PATH, LOG_DIR_PATH, CRAWL_ONLY_TEN_PLACES_FOR_TEST, COLLECTED_CATEGORY_DIR_PATH)
+    DO_CRAWL = False
+    DO_COMPUTE_DIFF = True 
 
-    
-    # save_only_diff(COLLECTED_CATEGORY_DIR_PATH , EXISTING_CATEGORY_TO_TAG_TABLE_DIR_PATH)
+    if DO_CRAWL:
+      crawl_only_category(STATIONS, SEARCH_KEYWORD, CRAWLER_OPTIONS, 
+      DB_DIR_PATH, PHOTO_DIR_PATH, LOG_DIR_PATH, CRAWL_ONLY_TEN_PLACES_FOR_TEST, COLLECTED_CATEGORY_DIR_PATH)
+
+    if DO_COMPUTE_DIFF:
+      save_only_diff(COLLECTED_CATEGORY_DIR_PATH , EXISTING_CATEGORY_TO_TAG_TABLE_DIR_PATH)
