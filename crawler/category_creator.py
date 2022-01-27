561cdb02667e6016b20c82766dd6f5ad124d913c
AnHaechan <mike000529@gmail.com>
2022-01-27 18:02:58 +0900

    220127 category creator : 'category_to-tag_table_other.csv' has been added & category_to_tag_table_*.csv updated with 5 stations : 구로디지털단지역, 홍대입구역, 선릉역, 잠실역, 강남역

--- crawler/category_creator.py
+++ crawler/category_creator.py
@@ -8,7 +8,9 @@ from category_crawler import crawl_only_category
 
 def save_only_diff(collected_category_dir_path, existing_category_to_tag_table_dir_path):
   entire_existing_categories= set()
-  for kind, kind_eng in [('술집', 'drink'), ('맛집', 'res'), ('카페','cafe')]:
+  # 'other' : 호텔같은 category ... 이미 한 번 봐서 category_to_tag_table_other.csv에 저장되어 있다면,
+  # new_cat_{kind}.csv 에 다시 들어가지 않게 제외시켜야 함
+  for kind_eng in ['drink', 'res', 'cafe', 'other']:
     existing_categories = set(csv_to_list(f"{existing_category_to_tag_table_dir_path}/category_to_tag_table_{kind_eng}.csv", encoding='cp949')[1:])
     entire_existing_categories.update(existing_categories)
 
@@ -34,14 +36,14 @@ if __name__ == "__main__":
     EXISTING_CATEGORY_TO_TAG_TABLE_DIR_PATH = "./cat_to_tag_table"
     create_dirs_if_not_exist([DB_DIR_PATH, PHOTO_DIR_PATH, LOG_DIR_PATH, COLLECTED_CATEGORY_DIR_PATH, EXISTING_CATEGORY_TO_TAG_TABLE_DIR_PATH])
 
-    STATIONS = ["구로디지털단지역", "홍대입구역", "선릉역", "잠실역", "강남역"] #[station+"역" for station in csv_to_list("./all_metro_list.csv")][124:]
+    STATIONS = [station+"역" for station in csv_to_list("./all_metro_list.csv")][129:] # ["구로디지털단지역", "홍대입구역", "선릉역", "잠실역", "강남역"]
     print(STATIONS)   
     SEARCH_KEYWORD = ['술집', '카페', '맛집']
     CRAWLER_OPTIONS = dict(log=True, msg=True)
     CRAWL_ONLY_TEN_PLACES_FOR_TEST = False
 
-    DO_CRAWL = False
-    DO_COMPUTE_DIFF = True
+    DO_CRAWL = True
+    DO_COMPUTE_DIFF = False
 
     if DO_CRAWL:
       crawl_only_category(STATIONS, SEARCH_KEYWORD, COLLECTED_CATEGORY_DIR_PATH, 'naver_get_restaurants_query.json')
