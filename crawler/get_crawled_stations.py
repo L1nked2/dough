7b07d2eea39483ef9b01913bf1673d5275656db3
AnHaechan <mike000529@gmail.com>
2022-02-23 17:09:31 +0900

    220223 crawler : get crawleds stations in multiple disks

--- crawler/get_crawled_stations.py
+++ crawler/get_crawled_stations.py
@@ -1,13 +1,15 @@
 import sys
 import os
   
-raw_db_path = sys.argv[1]
-assert len(sys.argv) == 2, "args must be `path/to/raw_db`, e.g. /media/k/DIR_FOR_CRAWLING/raw_db"
+assert len(sys.argv) == 3, "python get_cralwed_stations_py <first_disk> <second_disk> e.g. /media/k /media/k2"
 
-crawled_station_categories = os.listdir(raw_db_path)
-crawled_stations_with_repetition = [station_category.split("_")[0] for station_category in crawled_station_categories]
-crawled_stations = list(set(crawled_stations_with_repetition))
-fully_crawled_stations = sorted(list(filter(lambda station : crawled_stations_with_repetition.count(station) == 3, crawled_stations)))
+fully_crawled_stations = []
+for disk_path in sys.argv[1:]:
+    raw_db_path = os.path.join(disk_path, "DIR_FOR_CRAWLING/raw_db")
+    crawled_station_categories = os.listdir(raw_db_path)
+    crawled_stations_with_repetition = [station_category.split("_")[0] for station_category in crawled_station_categories]
+    crawled_stations = list(set(crawled_stations_with_repetition))
+    fully_crawled_stations += sorted(list(filter(lambda station : crawled_stations_with_repetition.count(station) == 3, crawled_stations)))
 
 print(f"cralwed stations are...\n{fully_crawled_stations}")
 
